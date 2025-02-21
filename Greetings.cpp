331c09d2bfcf4478b718ebf429a5825bb5a7a3be
nevermore0665 <nevermore0665@gmail.com>
2025-02-20 18:26:02 -0600

    Add Greeting.cpp

---
+++ Greetings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string Name;
+    cout << "Enter an your name: ";
+    cin >> Name;
+    cout << "It is nice to meet you," << Name << "!";
+
+return 0;
+}
